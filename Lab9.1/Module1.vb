334836cf8164a9b19e21196ea07eae39750d64e4
tonypiwko <tony.piwko@outlook.com>
2015-11-10 20:16:42 -0500

    Added steps 9-13 of lab, and confirmed it works per lab instructions.

--- Lab9.1/Module1.vb
+++ Lab9.1/Module1.vb
@@ -3,15 +3,20 @@
     Sub Main()
         Dim number As Integer = 0
         Dim increment As Integer = 10
-        Sum(number, increment)
+        Dim myNumber As Double = 0
+        Dim squareRoot As Double = 0
+        addTen(number, increment)
         Console.WriteLine("Your number with 10 added to it is " & number)
+        Console.WriteLine("")
+        sqrt(myNumber, squareRoot)
+        Console.WriteLine("The square root of your number is " & squareRoot)
         Console.WriteLine("")
         Console.WriteLine("Press enter key to exit...")
         Console.ReadLine()
 
     End Sub
 
-    Function Sum(ByRef number As Integer, ByVal increment As Integer) As Integer
+    Function addTen(ByRef number As Integer, ByVal increment As Integer) As Integer
 
         'number entry prompt
         Console.WriteLine("Please enter the number you wish to start with.")
@@ -23,6 +28,14 @@
 
     End Function
 
+    Function sqrt(ByRef myNumber As Double, ByRef squareRoot As Double)
+
+        'square root entry prompt
+        Console.WriteLine("Please enter the number you wish to find the square root of.")
+        myNumber = Console.ReadLine()
+        squareRoot = System.Math.Sqrt(myNumber)
+        Return squareRoot
+    End Function
 End Module
 
 
